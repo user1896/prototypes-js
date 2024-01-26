7ee4fa7e99ffe90a7be3f733ef9d1a9f1ea0b193
user1896 <abde.user15@gmail.com>
2024-01-26 21:48:14 +0100

    make the modal disppear when we click anywhere in the page

--- highlight_modal/script.js
+++ highlight_modal/script.js
@@ -1,12 +1,20 @@
 function click_btn(){
 	const modal = document.getElementById("modal-container")
-	modal.style.display = "block"
+	modal.style.display = "flex"
 }
 
 function click_close_btn(){
 	const modal = document.getElementById("modal-container")
 	modal.style.display = "none"
 }
+
+window.addEventListener("click", function(e){
+	const modal = document.getElementById("modal-container")
+	const btn = document.getElementById("open-btn")
+	if((e.target != modal) && (e.target != btn)){
+		modal.style.display = "none"
+	}
+})
 
 const btn = document.getElementById("open-btn")
 btn.addEventListener("click", click_btn)
