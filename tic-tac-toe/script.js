7ac9af1b6d940d11e36577dcafd492625a340bd0
user1896 <abde.user15@gmail.com>
2024-02-26 01:45:51 +0100

    finishing the game's logic

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -3,7 +3,7 @@ const main = document.querySelector("main")
 console.log(main.children)
 
 const matrix = [
-	['x'],[null],[null],
+	[null],[null],[null],
 	[null],['x'],[null],
 	['x'],[null],['o']
 ]
@@ -42,7 +42,15 @@ function how_many_marks_in_lineup( lineup, playersMark ){
 	return countMarks
 }
 
-console.log( how_many_marks_in_lineup( lineups[6], 'x' ) )
+function count_empty_spots( lineup ){
+	let empty_spots = 0
+	for( let i = 0; i < 3; i++ ){
+		if( !lineup[i][0] ){
+			empty_spots++
+		}
+	}
+	return empty_spots
+}
 
 function is_lineup_can_be_marked( lineup ){
 	for( let i = 0; i < 3; i++ ){
@@ -59,20 +67,42 @@ function is_lineup_can_be_marked( lineup ){
 	}
 }
 
-console.log( is_lineup_can_be_marked(lineups[6]) )
-
-function best_lineup_to_mark( playersMark ){
+function best_lineup_to_mark( playersMark, opponentsMark ){
 	let chosen_lineup = 0
 	let position = 0
 	let countMarks = 0
+	let empty_spots = 0
 	for( let i = 0; i < 8; i++ ){
-		const tmp_countMarks = how_many_marks_in_lineup(lineups[i], playersMark)
+		const players_current_lineup_countMarks = how_many_marks_in_lineup(lineups[i], playersMark)
+		const opponents_current_lineup_countMarks = how_many_marks_in_lineup(lineups[i], opponentsMark)
 		const {is_can_be_marked, position_to_mark} = is_lineup_can_be_marked(lineups[i])
+		const current_lineup_empty_spots = count_empty_spots(lineups[i])
 
-		if( (tmp_countMarks > countMarks) && (is_can_be_marked === true) ){
-			countMarks = tmp_countMarks
+		// If opponent is about to win (has 2 marks), then we return this lineup to stop him from winning
+		if( is_can_be_marked && (opponents_current_lineup_countMarks === 2) ){
+			// opponent can have two marks but the third sport is taken by the player, so we check "if(is_can_be_marked)" first
+			return {
+				chosen_lineup: i,
+				position: position_to_mark
+			}
+		}
+
+		if( is_can_be_marked && 
+			// we can have two marks but the third sport is taken by opponent, so we check "if(is_can_be_marked)" first
+			(players_current_lineup_countMarks >= countMarks) && 
+			// if player has two marks then this lineup should be taken instead of a lineup that has only one mark.
+			(current_lineup_empty_spots > empty_spots)){
+				// if a player had one mark in the lineup with two empty spots, then it's better than one mark with only one 
+				// empty spot (because the oponent took the other).
+			empty_spots = current_lineup_empty_spots
+			countMarks = players_current_lineup_countMarks
 			chosen_lineup = i
 			position = position_to_mark
+			if( ((i === 1) || (i === 4) || (i === 6) || (i === 7)) && !lineups[i][1][0] ){
+				// if we're at a lineup that crose the middle of the matrix, and if the middle is empty, because the player will
+				// have move advantage if we cross the middle of the matrix to win the game.
+				position = 1
+			}
 		}
 	}
 	return {
@@ -81,20 +111,11 @@ function best_lineup_to_mark( playersMark ){
 	}
 }
 
-console.log( best_lineup_to_mark('x') )
-
-function play( playersMark ){
-	const {chosen_lineup, position} = best_lineup_to_mark(playersMark)
+function play( playersMark, opponentsMark ){
+	const {chosen_lineup, position} = best_lineup_to_mark(playersMark, opponentsMark)
 	lineups[chosen_lineup][position][0] = playersMark
 }
 
-// play('x')
-// debugger
-play('o')
-// we need to count for when there are more free spaces in a lineup
-// also count for when the opponent has 2, so we need to stop him from winning
-display()
+// const ifWinner = (player) => {
 
-const ifWinner = (player) => {
-
-}
+// }
