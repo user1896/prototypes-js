6b17c337d78202d995aa0eda568180574c1ce830
user1896 <abde.user15@gmail.com>
2024-02-27 18:53:04 +0100

    games logic where the machine stops opponent from winning instead of taking the win is fixed

--- tic-tac-toe/modules/games-logic/bestLineup.mjs
+++ tic-tac-toe/modules/games-logic/bestLineup.mjs
@@ -2,37 +2,46 @@ import lineups from '../lineups.mjs'
 import how_many_marks_in_lineup from './countMarks.mjs'
 import is_lineup_can_be_marked from './isCanBeMarked.mjs'
 
-// fix this:
-// right now if I'm about to win and the opponent is also about to win, I stop him instead of winning myself
-// to make this we need to store the information, because for example the opponent can have 2 marks in lineup 1,
-// and I have 2 marks in lineup 4, here I should not return the result in lineup 1 when the opponent is winning,
-// instead I read all other lineups to see if I'm winning before making a decision, so these info should stored
 function best_lineup_to_mark( playersMark, opponentsMark ){
 	let chosen_lineup = 0
 	let position = 0
 	let players_countMarks = 0
 	let opponents_countMarks = 2
+	let is_opponent_about_to_win = false
 	for( let i = 0; i < 8; i++ ){
 		const players_current_lineup_countMarks = how_many_marks_in_lineup(lineups[i], playersMark)
 		const opponents_current_lineup_countMarks = how_many_marks_in_lineup(lineups[i], opponentsMark)
 		const {is_can_be_marked, position_to_mark} = is_lineup_can_be_marked(lineups[i])
 
-		// If opponent is about to win (has 2 marks), then we return this lineup to stop him from winning
-		if( is_can_be_marked && (opponents_current_lineup_countMarks === 2) ){
-			// opponent can have two marks but the third spot is taken by the player, so we check "if(is_can_be_marked)" first
+		// If the player is about to win, then we return immediately
+		if( is_can_be_marked && (players_current_lineup_countMarks === 2) ){
+		// The player can have two marks while the opponent has the third spot, so we check if "is_can_be_marked" first
 			return {
 				chosen_lineup: i,
 				position: position_to_mark
 			}
 		}
 
-		if( is_can_be_marked
+		// If the opponent is about to win (has 2 marks), then store this lineup's info
+		if( is_can_be_marked && (opponents_current_lineup_countMarks === 2) ){
+			// opponent can have two marks but the third spot is taken by the player, so we check "if(is_can_be_marked)" first
+			chosen_lineup = i
+			position = position_to_mark
+			is_opponent_about_to_win = true
+		}
+
+		if( (is_opponent_about_to_win === false)
+			// if "is_opponent_about_to_win == true" then nothing matters until we find a lineup where we have two marks to
+			// take the win, or if we can't win, then we return the lineup where the opponent is about to win to stop him.
+			
+			&& is_can_be_marked
 			// we can have two marks but the third spot could be taken by the opponent, so we check "if(is_can_be_marked)" first
+			
 			&& (players_current_lineup_countMarks >= players_countMarks)
-			// if player has two marks then this lineup should be taken instead of a lineup that has only one mark.
+			// if the player has one mark then this lineup should be taken over a lineup that has no marks.
 			
 			&& (opponents_current_lineup_countMarks < opponents_countMarks)){
-				// if a player had one mark in the lineup with two empty spots, then it's better than one mark with only one 
+				// if the player has one mark in the lineup with two empty spots, then it's better than one mark with only one 
 				// empty spot (because the oponent took the other).
 			
 			players_countMarks = players_current_lineup_countMarks
--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -14,3 +14,7 @@ reset_btn.addEventListener("click", function(){
 	reset()
 	start_game()
 })
+// add the ability to choose your mark ('X' or 'O')
+// when I mark my spot, make a little deley (for ex 500ms) before the machine mark hers
+// When someone wins, the lineup that has 3 marks should be crossed
+// When no one wins we should display "Draw" and reset the game
