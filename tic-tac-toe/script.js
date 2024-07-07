260b9e0e16c34a532b61f35d47f4ed0e3a7bce86
user1896 <abde.user15@gmail.com>
2024-07-07 23:05:39 +0200

    notes to switch to ReactJs

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -17,3 +17,27 @@ reset_btn.addEventListener("click", function(){
 // when I mark my spot, make a little deley (for ex 500ms) before the machine mark hers
 // animate the marks and the cross-line
 // add the ability to choose your mark ('X' or 'O')
+
+/*
+# What is done:
+
+# games-logic:
+
+bestLineup => in hooks: usePositionToMark
+countMarks => in games-logic
+isCanBeMarked => in games-logic
+isWinner => in hooks: useWinner
+play => in hooks: usePlay
+
+
+# other modules:
+
+crossLineup : in components: CrossLineup.js
+handleClick => mixed logic in Grid.js and Spot.js
+lineups => in hooks: useLineups.js
+lineupToMatrix => in utils
+matrix => in context: matrixContext.js
+postWinning: mixed logic in App.js and calling crossLineup
+reset => in components: ResetButton.js
+
+*/
